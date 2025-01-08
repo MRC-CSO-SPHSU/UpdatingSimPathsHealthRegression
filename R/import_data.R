45c68e34cdbd60ec5f7e832d20657c4d991c2793
Andy Baxter <andrewbaxter439@gmail.com>
2025-01-08 10:30:27 +0000

    seems to be lagging correctly!

--- R/import_data.R
+++ R/import_data.R
@@ -10,7 +10,7 @@ library(haven)
 data <- read_dta('T:/projects/HEED/DataAnalysis/hfcovid_analysis.dta',
                  col_select = c(pidp, hidp, wave,
                                 sex_dv, age_dv, age_sq, hiqual_dv,
-                                scsf1, sclfsato, sf12pcs_dv, sf12mcs_dv,
+                                scsf1, sclfsato, sf12pcs_dv, sf12mcs_dv, scghq1_dv,
                                 gor_dv,
                                 pidp,
                                 intdaty_dv,
@@ -40,7 +40,7 @@ codes_translate <-
 
 
 data_ready <- data |> 
-  transmute(
+  mutate(
     pidp = pidp,
     D_Econ_benefits = econ_benefits,
     D_Home_owner = home_owner,
@@ -58,36 +58,121 @@ data_ready <- data |>
     Deh_c3 = as_factor(hiqual_dv) |> fct_relabel(~str_extract(.x, "(?<=: ).*")),
     Year_transformed = intdaty_dv |> zap_labels(),
     Dls = sclfsato,
-    Dwb_mcs = sf12mcs_dv,
-    Dwb_pcs = sf12pcs_dv,
+    Dhm = scghq1_dv,
+    Dhe_mcs = sf12mcs_dv,
+    Dhe_pcs = sf12pcs_dv,
     weight = indscus_lw,
     les_c4 = case_when(
       jbstat %in% c(1, 2, 5, 12, 13, 14) ~ "Employed or self-employed",
       jbstat == 7 ~ "Student",
       jbstat %in% c(3, 6, 8, 10, 11, 97, 9) ~ "Not employed",
       jbstat == 4 ~ "Retired"
-    ),
-    EmployedToUnemployed = as.integer(lag(les_c4) == "Employed or self-employed" & les_c4 == "Not employed" & dlltsd == 0),
-    UnemployedToEmployed = as.integer(lag(les_c4) == "Not employed" & les_c4 == "Employed or self-employed" & dlltsd == 0),
-    PersistentUnemployed = as.integer(lag(les_c4) == "Not employed" & les_c4 == "Not employed" & dlltsd == 0),
-    NonPovertyToPoverty = as.integer(lag(econ_poverty) == 0 & econ_poverty == 1),
-    PovertyToNonPoverty = as.integer(lag(econ_poverty) == 1 & econ_poverty == 0),
-    PersistentPoverty = as.integer(lag(econ_poverty) == 1 & econ_poverty == 1),
-    RealIncomeChange = log_income - lag(log_income),
-    RealIncomeDecrease_D = as.integer(econ_realequivinc < lag(econ_realequivinc)),
-    Covid_2020_D = as.integer(intdaty_dv == 20),
-    Covid_2021_D = as.integer(intdaty_dv == 21),
+    )
+    # EmployedToUnemployed = as.integer(lag(les_c4) == "Employed or self-employed" & les_c4 == "Not employed" & dlltsd == 0),
+    # UnemployedToEmployed = as.integer(lag(les_c4) == "Not employed" & les_c4 == "Employed or self-employed" & dlltsd == 0),
+    # PersistentUnemployed = as.integer(lag(les_c4) == "Not employed" & les_c4 == "Not employed" & dlltsd == 0),
+    # NonPovertyToPoverty = as.integer(lag(econ_poverty) == 0 & econ_poverty == 1),
+    # PovertyToNonPoverty = as.integer(lag(econ_poverty) == 1 & econ_poverty == 0),
+    # PersistentPoverty = as.integer(lag(econ_poverty) == 1 & econ_poverty == 1),
+    # RealIncomeChange = log_income - lag(log_income),
+    # RealIncomeDecrease_D = as.integer(econ_realequivinc < lag(econ_realequivinc)),
   ) |> 
   left_join(codes_translate, by = "gor_dv") |> 
-  mutate(across(c(Dag, Dgn, Dls, Dwb_mcs, Dwb_pcs, Dhe, D_Econ_benefits, Dlltsd, D_Home_owner), zap_labels)) |> 
+  mutate(across(c(Dag, Dgn, Dls, Dhe_mcs, Dhe_pcs, Dhe, D_Econ_benefits, Dlltsd, D_Home_owner), zap_labels)) |> 
   mutate(drgnl = relevel(factor(drgnl), ref = "UKI")) |> 
-  drop_na() |> 
   select(-gor_dv)
 
 ## lagged variables to be used as confounders: econ_benefits, home_owner, mastat_dv, dnc, gor_dv, and outcome measures
 
-data_with_dummies_lags <- data_ready |> 
+data_with_dummies <- data_ready |> 
   fastDummies::dummy_cols(select_columns = c("Ydses_c5"), remove_first_dummy = TRUE) |> 
   fastDummies::dummy_cols(select_columns = c("Deh_c3", "Dcpst"), remove_first_dummy = TRUE) |> 
-  fastDummies::dummy_cols(select_columns = c("drgnl"), remove_first_dummy = TRUE, omit_colname_prefix = TRUE) |> 
-  mutate(across(everything(), lag, .names = "{.col}_L1"), Constant = 1) 
+  fastDummies::dummy_cols(select_columns = c("drgnl"), remove_first_dummy = TRUE, omit_colname_prefix = TRUE)
+
+library(dtplyr)
+
+lagged_vars <- c(
+  "Dnc", "Ydses_c5_Q2", "Ydses_c5_Q3", "Ydses_c5_Q4", "Ydses_c5_Q5", "Dlltsd", "Dag", "Dag_sq",
+  "Dhe_mcs", "Dhe_pcs", "Dhm", "Dls",
+  "les_c4", "econ_poverty", "log_income", "econ_realequivinc"
+)
+
+lagged_names <- paste0(lagged_vars, "_L1")
+
+data_with_dummies_dt <- as.data.table(data_with_dummies)
+
+data_with_dummies_dt[, (lagged_names) := shift(.SD), by = pidp, .SDcols = lagged_vars]
+
+data_with_dummies_dt[, `:=`(
+  EmployedToUnemployed = as.integer(
+    les_c4_L1 ==
+      "Employed or self-employed" &
+      les_c4 == "Not employed" &
+      dlltsd == 0
+  ),
+  UnemployedToEmployed = as.integer(
+    les_c4_L1 ==
+      "Not employed" &
+      les_c4 == "Employed or self-employed" &
+      dlltsd == 0
+  ),
+  PersistentUnemployed = as.integer(les_c4_L1 ==
+                                      "Not employed" &
+                                      les_c4 == "Not employed" & dlltsd == 0),
+  NonPovertyToPoverty = as.integer(econ_poverty_L1 == 0 &
+                                     econ_poverty ==
+                                     1),
+  PovertyToNonPoverty = as.integer(econ_poverty_L1 ==
+                                     1 &
+                                     econ_poverty == 0),
+  PersistentPoverty = as.integer(econ_poverty_L1 ==
+                                   1 &
+                                   econ_poverty == 1),
+  RealIncomeChange = log_income -
+    log_income_L1,
+  RealIncomeDecrease_D = as.integer(econ_realequivinc <
+                                      econ_realequivinc_L1)
+)]
+
+data_with_dummies_lags <- data_with_dummies_dt |> 
+  as_tibble() |> 
+  select(
+    pidp,
+    Year_transformed,
+    weight,
+    Dgn, 
+    Dnc,
+    Dnc_L1,
+    starts_with("Ydses", ignore.case = FALSE),
+    Dlltsd,
+    Dlltsd_L1,
+    Dag,
+    Dag_L1,
+    Dag_sq,
+    Dag_sq_L1,
+    Dhe_mcs,
+    Dhe_mcs_L1,
+    Dhe_pcs,
+    Dhe_pcs_L1,
+    Dhm,
+    Dhm_L1,
+    Dls,
+    Dls_L1,
+    EmployedToUnemployed,
+    UnemployedToEmployed,
+    PersistentUnemployed,
+    NonPovertyToPoverty,
+    PovertyToNonPoverty,
+    PersistentPoverty,
+    RealIncomeChange,
+    RealIncomeDecrease_D,
+    starts_with("UK", ignore.case = FALSE),
+    D_Econ_benefits,
+    D_Home_owner,
+    Dcpst_Single,
+    Dcpst_PreviouslyPartnered,
+    starts_with("Deh", ignore.case = FALSE),
+    -ends_with("NA"),
+  ) |> 
+  mutate(Constant = 1) |> 
+  drop_na()
